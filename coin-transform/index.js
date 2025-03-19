472fd77b5fd20a8edbbf0b1ec159fe0ca2634ef2
João <Joaopauloresendecosta55@gmail.com>
2025-03-19 14:26:39 -0300

    identificando a moeda

--- coin-transform/index.js
+++ coin-transform/index.js
@@ -1,4 +1,7 @@
 // capturando os elementos do formulario
+const moneyUSD = 5.65
+const moneyEUR = 6.15
+const moneyGBP = 7.33
 const input = document.querySelector('#amount');
 const coins = document.querySelector('#currency')
 const form = document.querySelector('form');
@@ -13,6 +16,22 @@ input.addEventListener('input', (ev) => {
 // capturando o submit do formulário
 form.addEventListener('submit', (ev) => {
     ev.preventDefault()
-    console.log(coins.value)
-
+    switch (coins.value) {
+        case 'USD':
+            convertCurrency(input.value, moneyUSD, '$')
+            break;
+        case 'EUR':
+            convertCurrency(input.value, moneyEUR, '€')
+            break;
+        case 'GBP':
+            convertCurrency(input.value, moneyGBP, '£')
+            break;
+        default:
+            break;
+    }
 })
+
+// função para converter a moeda
+function convertCurrency(amount, price, symbol) {
+   
+}
